d5e5aa14eaf27b1987e2b3a7a4513effe6a6e382
Pavol Rusnak <stick@gk2.sk>
2016-03-03 11:17:00 +0100

    don't use config, use python module instead to hold definitions

---
+++ example.py
@@ -0,0 +1,28 @@
+AUTHOR = 'Pavol Rusnak'
+
+def func_a():
+    pass
+
+def func_b(arg1):
+    pass
+
+def func_c(arg1, arg2):
+    pass
+
+def func_d(arg1, arg2, arg3):
+    pass
+
+def func_e(arg1, arg2, arg3, arg4):
+    pass
+
+def func_f(arg1, arg2, arg3, arg4, arg5):
+    pass
+
+def func_g(arg1, arg2, arg3=None, arg4=None):
+    pass
+
+def func_h(arg1, arg2, arg3, *args):
+    pass
+
+def func_i(arg1, arg2, **kwargs):
+    pass
