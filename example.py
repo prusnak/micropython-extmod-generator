e62e23e75dc23ce5252609904569c65953d1caa4
Pavol Rusnak <stick@gk2.sk>
2016-03-03 16:27:58 +0100

    implement classes + methods

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 _author = 'Pavol Rusnak'
 
+
 def func_a():
     pass
 
@@ -26,3 +27,36 @@ def func_h(arg1, arg2, arg3, *args):
 
 def func_i(arg1, arg2, **kwargs):
     pass
+
+
+class Klass(object):
+
+    def __init__(self):
+        pass
+
+    def func_a(self):
+        pass
+
+    def func_b(self, arg1):
+        pass
+
+    def func_c(self, arg1, arg2):
+        pass
+
+    def func_d(self, arg1, arg2, arg3):
+        pass
+
+    def func_e(self, arg1, arg2, arg3, arg4):
+        pass
+
+    def func_f(self, arg1, arg2, arg3, arg4, arg5):
+        pass
+
+    def func_g(self, arg1, arg2, arg3=None, arg4=None):
+        pass
+
+    def func_h(self, arg1, arg2, arg3, *args):
+        pass
+
+    def func_i(self, arg1, arg2, **kwargs):
+        pass
