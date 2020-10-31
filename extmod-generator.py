4be8981fa809a994b53ad2a79099c96926276ec2
IhorNehrutsa <Ihor.Nehrutsa@gmail.com>
2020-11-01 01:24:31 +0200

    Add converting PYthon parameters to C variables Add return result

--- extmod-generator.py
+++ extmod-generator.py
@@ -369,6 +369,8 @@ def generate_function(src, f):
     if f.func.__doc__ is not None:
         src.append('/*' + format_comment(f.func.__doc__) + '*/')
 
+    sig = inspect.signature(f.func)
+
     # special case for constructor
     if f.name == '__init__':
         src.append('STATIC mp_obj_t {module}_{classname}_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {{', classname=f.classname)
@@ -392,6 +394,7 @@ def generate_function(src, f):
         src.append('    mp_obj_{module}_{classname}_t *self = m_new_obj(mp_obj_{module}_{classname}_t);', classname=f.classname)
         src.append('    self->base.type = &{module}_{classname}_type;', classname=f.classname)
         src.append("")
+        src.append_str(parse_params(f, sig.parameters))
         src.append(code(f))
         src.append("")
         src.append('    return MP_OBJ_FROM_PTR(self);')
@@ -409,25 +412,35 @@ def generate_function(src, f):
         src.append('')
         return
 
-    if f.type == '0':
-        src.append('STATIC mp_obj_t {module}_{function}(void) {{', function=f.fullname)
-    elif f.type == '1':
-        src.append('STATIC mp_obj_t {module}_{function}(mp_obj_t {args[0]}) {{', function=f.fullname, args=f.argspec.args)
-    elif f.type == '2':
-        src.append('STATIC mp_obj_t {module}_{function}(mp_obj_t {args[0]}, mp_obj_t {args[1]}) {{', function=f.fullname, args=f.argspec.args)
-    elif f.type == '3':
-        src.append('STATIC mp_obj_t {module}_{function}(mp_obj_t {args[0]}, mp_obj_t {args[1]}, mp_obj_t {args[2]}) {{', function=f.fullname, args=f.argspec.args)
-    elif f.type == 'var':
-        src.append('STATIC mp_obj_t {module}_{function}(size_t n_args, const mp_obj_t *args) {{', function=f.fullname)
-    elif f.type == 'var_between':
-        src.append('STATIC mp_obj_t {module}_{function}(size_t n_args, const mp_obj_t *args) {{', function=f.fullname)
-    elif f.type == 'kw':
-        src.append('STATIC mp_obj_t {module}_{function}(size_t n_args, const mp_obj_t *args, mp_map_t *kw_args) {{', function=f.fullname)
-    else:
-        raise Exception('Unknown function type: {type}'.format(type=f.type))
+#     if f.type == '0':
+#         src.append('STATIC mp_obj_t {module}_{function}(void) {{', function=f.fullname)
+#     elif f.type == '1':
+#         src.append('STATIC mp_obj_t {module}_{function}(mp_obj_t {args[0]}_obj) {{', function=f.fullname, args=f.argspec.args)
+#     elif f.type == '2':
+#         src.append('STATIC mp_obj_t {module}_{function}(mp_obj_t {args[0]}_obj, mp_obj_t {args[1]}_obj) {{', function=f.fullname, args=f.argspec.args)
+#     elif f.type == '3':
+#         src.append('STATIC mp_obj_t {module}_{function}(mp_obj_t {args[0]}_obj, mp_obj_t {args[1]}_obj, mp_obj_t {args[2]}_obj) {{', function=f.fullname, args=f.argspec.args)
+#     elif f.type == 'var':
+#         src.append('STATIC mp_obj_t {module}_{function}(size_t n_args, const mp_obj_t *args) {{', function=f.fullname)
+#     elif f.type == 'var_between':
+#         src.append('STATIC mp_obj_t {module}_{function}(size_t n_args, const mp_obj_t *args) {{', function=f.fullname)
+#     elif f.type == 'kw':
+#         src.append('STATIC mp_obj_t {module}_{function}(size_t n_args, const mp_obj_t *args, mp_map_t *kw_args) {{', function=f.fullname)
+#     else:
+#         raise Exception('Unknown function type: {type}'.format(type=f.type))
 
-    src.append('    // TODO')
-    src.append('    return mp_const_none;')
+    s = function_init(f"{f.func.__module__}_{f.fullname}")
+    s += function_params(f, sig.parameters)
+    src.append_str(s)
+
+    src.append_str(parse_params(f, sig.parameters))
+
+    ret_init = ret_val_init(sig)
+    if ret_init:
+        src.append(ret_init)
+
+#     src.append('    // TODO')
+#     src.append('    return mp_const_none;')
     
     src.append("")
     src.append(code(f))
@@ -464,10 +477,6 @@ def generate_class(src, c):
     src.append('// class {classname}({parents}):', classname=c.name, parents=parents)
     if c.class_instance.__doc__ is not None:
         src.append('/*' + format_comment(c.class_instance.__doc__) + '*/')
-    src.append('typedef struct _mp_obj_{classname}_t {{', classname=c.name)
-    src.append('    mp_obj_base_t base;')
-    src.append('}} mp_obj_{classname}_t;', classname=c.name)
-    src.append('')
 
     if len(c.defines) > 0:
         src.append('// {classname} constants', classname=c.name)
@@ -622,6 +631,204 @@ def main():
 
     module = Module(args.module)
     generate(module, args.force_overwrite)
+
+#= uStubby based ===========================================================================================
+def string_template(base_str):
+    def string_handle(*args, **kwargs):
+        return base_str.format(*args, **kwargs)
+    return string_handle
+ 
+in_type_handler = {
+    int: string_template("\tmp_int_t {0} = mp_obj_get_int({0}_obj);"),
+    float: string_template("\tmp_float_t {0} = mp_obj_get_float({0}_obj);"),
+    bool: string_template("\tbool {0} = mp_obj_is_true({0}_obj);"),
+    str: string_template("\tconst char* {0} = mp_obj_str_get_str({0}_obj);"),
+    tuple: string_template("\tmp_obj_t *{0} = NULL;\n\tsize_t {0}_len = 0;\n\tmp_obj_get_array({0}_arg, &{0}_len, &{0});"),
+    list: string_template("\tmp_obj_t *{0} = NULL;\n\tsize_t {0}_len = 0;\n\tmp_obj_get_array({0}_arg, &{0}_len, &{0});"),
+    set: string_template("\tmp_obj_t *{0} = NULL;\n\tsize_t {0}_len = 0;\n\tmp_obj_get_array({0}_arg, &{0}_len, &{0});"),
+    object: string_template("\tmp_obj_t {0} args[ARG_{0}].u_obj;"),
+    'self': string_template("\tmp_obj_{1}_{2}_t *{0} = MP_OBJ_TO_PTR({0}_obj);"),
+    inspect._empty: string_template("qqq"),
+    None: string_template("www")
+    }
+
+in_type_handler_arr = {
+    int: string_template("\tmp_int_t {0} = mp_obj_get_int(args[{3}]);"),
+    float: string_template("\tmp_float_t {0} = mp_obj_get_float(args[{3}]);"),
+    bool: string_template("\tbool {0} = mp_obj_is_true(args[{3}]);"),
+    str: string_template("\tconst char* {0} = mp_obj_str_get_str(args[{3}]);"),
+    tuple: string_template("\tmp_obj_t *{0} = NULL;\n\tsize_t {0}_len = 0;\n\tmp_obj_get_array(args[{3}], &{0}_len, &{0});"),
+    list: string_template("\tmp_obj_t *{0} = NULL;\n\tsize_t {0}_len = 0;\n\tmp_obj_get_array(args[{3}], &{0}_len, &{0});"),
+    set: string_template("\tmp_obj_t *{0} = NULL;\n\tsize_t {0}_len = 0;\n\tmp_obj_get_array(args[{3}], &{0}_len, &{0});"),
+    object: string_template("\tmp_obj_t {0} args[ARG_{0}].u_obj;"),
+    'self': string_template("\tmp_obj_{1}_{2}_t *{0} = MP_OBJ_TO_PTR(args[{3}]);"),
+    inspect._empty: "eee",
+    None: "rrr"
+    }
+
+return_type_handler = {
+    int: "\tmp_int_t ret_val;",
+    float: "\tmp_float_t ret_val;",
+    bool: "\tbool ret_val;",
+    str: "",
+    bytes: "",
+    tuple: "",
+    list: "",
+    dict: "",
+    object: "",
+    set: "",
+    inspect._empty: "",
+    None: ""
+    }
+
+return_handler = {
+    int: "\treturn mp_obj_new_int(ret_val);",
+    float: "\treturn mp_obj_new_float(ret_val);",
+    bool: "\treturn mp_obj_new_bool(ret_val);",
+    str: """\t// signature: mp_obj_t mp_obj_new_bytes(const char* data, size_t len);
+    return mp_obj_new_str("hello", 5);""",
+    tuple: '''\t// signature: mp_obj_t mp_obj_new_tuple(size_t n, const mp_obj_t *items);
+    mp_obj_t ret_val[] = {
+        mp_obj_new_int(123),
+        mp_obj_new_float(456.789),
+        mp_obj_new_str("hello", 5),
+    };
+    return mp_obj_new_tuple(3, ret_val);''',
+    bytes: '''\t// signature: mp_obj_t mp_obj_new_bytes(const byte* data, size_t len);
+    return mp_obj_new_bytes("hello", 5);''',
+    list: '''\t// signature: mp_obj_t mp_obj_new_list(size_t n, const mp_obj_t *items);
+    mp_obj_t ret_val[] = {
+        mp_obj_new_int(123),
+        mp_obj_new_float(456.789),
+        mp_obj_new_str("hello", 5),
+    };
+    return mp_obj_new_list(3, ret_val);''',
+    dict: '''\tmp_obj_t ret_val = mp_obj_dict(0);
+    mp_obj_dict_store(ret_val, mp_obj_new_str("element1", 8), mp_obj_new_int(123));
+    mp_obj_dict_store(ret_val, mp_obj_new_str("element2", 8), mp_obj_new_float(456.789));
+    mp_obj_dict_store(ret_val, mp_obj_new_str("element3", 8), mp_obj_new_str("hello", 5));
+    return ret_val;''',
+    object: '''\treturn mp_const_none; // TODO''',
+    inspect._empty: "\treturn mp_const_none;",
+    None: "\treturn mp_const_none;"
+    }
+
+shortened_types = {
+    int: "int",
+    object: "obj",
+    bool: "bool",
+    None: "null",
+    inspect._empty: 'aaa'
+    }
+
+def in_type_init(name, _type, default):
+    if name == 'self':
+        return in_type_handler['self']
+    if _type == inspect._empty:
+        return in_type_handler_arr[type(default)]
+    return in_type_handler[_type]
+
+def in_type_init_arr(name, _type, default):
+    if name == 'self':
+        return in_type_handler_arr['self']
+    if _type == inspect._empty:
+        return in_type_handler_arr[type(default)]
+    return in_type_handler_arr[_type]
+
+def ret_val_init(sig):
+    return return_type_handler[sig.return_annotation]
+
+def ret_val_return(ret_type):
+    return return_handler[ret_type]
+
+def kw_enum(params):
+    return f"\tenum {{ {', '.join(['ARG_' + k for k in params])} }};"
+
+def kw_allowed_args(f, params):
+    args = []
+    for name, param in params.items():
+        if param.kind in [param.POSITIONAL_OR_KEYWORD, param.POSITIONAL_ONLY]:
+            arg_type = "MP_ARG_REQUIRED"
+        else:
+            arg_type = "MP_ARG_KW_ONLY"
+        type_txt = shortened_types[param.annotation]
+        if param.default is inspect._empty:
+            default = ""
+        elif param.default is None:
+            default = f"{{ .u_{type_txt} = MP_OBJ_NULL }}"
+        else:
+            default = f"{{ .u_{type_txt} = {param.default} }}"
+        args.append(f"{{ MP_QSTR_{name}, {arg_type} | MP_ARG_{type_txt.upper()}, {default} }},")
+    args = "\n\t\t".join(args)
+    return f"\n\tSTATIC const mp_arg_t {f.__module__}_{f.__name__}_allowed_args[] = {{\n\t\t{args}\n\t}};"
+
+def arg_array(f):
+    args = f"{f.__module__}_{f.__name__}_allowed_args"
+    return f"\tmp_arg_val_t args[MP_ARRAY_SIZE({args})];\n" \
+        f"\tmp_arg_parse_all(n_args - 1, pos_args + 1, kw_args,\n" \
+        f"\t\tMP_ARRAY_SIZE({args}), {args}, args);"
+
+
+def arg_unpack(params):
+    return "\n".join(f"\tmp_{shortened_types[param.annotation]}_t {name} = args[ARG_{name}].u_{shortened_types[param.annotation]};" for name, param in params.items())
+
+
+def function_init(func_name):
+    return f"STATIC mp_obj_t {func_name}("
+
+
+def function_params(f, params):
+    if f.type == '0':        
+        return "void) {"
+    elif f.type in ('1', '2', '3'):
+        params = ", ".join([f"mp_obj_t {x}_obj" for x in params])
+        return params + ") {"
+    elif f.type in ['var', 'var_between']:
+        return "size_t n_args, const mp_obj_t *args) {"
+    elif f.type == 'kw':
+        return "size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {"
+    else:
+        raise Exception('Unknown function type: {type}'.format(type=f.type))
+
+
+def ordereddict_to_dict(value):
+    for k, v in value.items():
+        if isinstance(v, dict):
+            value[k] = ordereddict_to_dict(v)
+    return dict(value)
+
+
+def parse_params(f, params):
+    """
+    :param params: Parameter signature from inspect.signature
+    :return: list of strings defining the parsed parameters in c
+    """
+    if f.type != '0':        
+        #for i, e in enumerate(params.items()):
+        for e in params.items():
+            param, value = e
+            if value.annotation == inspect._empty: # and value.default == inspect._empty:
+                if (param == 'self'): 
+                    pass
+                elif value.default != inspect._empty:
+                    pass
+                else:
+                    print('Please annotate type of parameter: File {module}, Line {line}, Function {function}, Parameter {parameter} Type {value}'.format(
+                        module=f.func.__code__.co_filename, line=f.func.__code__.co_firstlineno, function=f.func.__name__, parameter=param, value=value.annotation))#__module__
+                    # raise TypeError()
+                    return None
+
+    if f.type == '0':        
+        return None
+    elif f.type in ['1', '2', '3']:
+        return ''.join([in_type_init(param, value.annotation, value.default)(param, f.func.__module__, f.classname)+'\n' for param, value in params.items()])
+    elif f.type in ['var', 'var_between']:
+        return ''.join([in_type_init_arr(param, value.annotation, value.default)(param, f.func.__module__, f.classname, ind)+'\n' for ind, (param, value) in enumerate(params.items())])
+    elif f.type == 'kw':
+        return ''.join([kw_enum(params), kw_allowed_args(f.func, params), "", arg_array(f.func), "", arg_unpack(params)])
+    else:
+        raise Exception('Unknown function type: {type}'.format(type=f.type))
+
 
 def code(f):
     try:
