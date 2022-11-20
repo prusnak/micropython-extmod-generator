67ffc63ddb2d267db595d6506a085ad84b616dbc
IhorNehrutsa <Ihor.Nehrutsa@gmail.com>
2022-11-20 02:28:54 +0200

    example.consts

--- example/consts/consts_test.py
+++ example/consts/consts_test.py
@@ -2,28 +2,18 @@ import consts
 
 print(type(consts.CONST_NONE), consts.CONST_NONE)
 print(type(consts.CONST_INT), consts.CONST_INT)
-print(type(consts.CONST_BOOL0), consts.CONST_BOOL0)
-print(type(consts.CONST_BOOL1), consts.CONST_BOOL1)
+print(type(consts.CONST_FALSE), consts.CONST_FALSE)
+print(type(consts.CONST_TRUE), consts.CONST_TRUE)
 print(type(consts.CONST_FLOAT), consts.CONST_FLOAT)
 print(type(consts.CONST_STR), consts.CONST_STR)
+print(type(consts.CONST_BYTES), consts.CONST_BYTES)
 print(type(consts.CONST_TUPLE), consts.CONST_TUPLE)
-print(type(consts.CONST_TUPLE_X), consts.CONST_TUPLE_X)
-print(type(consts.CONST_TUPLE_X[0]), consts.CONST_TUPLE_X[0])
-
-print(type(consts.CONST_NONE), consts.CONST_NONE)
-#consts.CONST_NONE = 'NoneStr'
-print(type(consts.CONST_NONE), consts.CONST_NONE)
-
-print('CONST_INT', type(consts.CONST_INT), consts.CONST_INT)
-#consts.CONST_INT += 100
-print('CONST_INT', type(consts.CONST_INT), consts.CONST_INT)
-
-print('consts.CONST_FLOAT', type(consts.CONST_FLOAT), consts.CONST_FLOAT)
-#consts.CONST_FLOAT += 15.15
-print('consts.CONST_FLOAT', type(consts.CONST_FLOAT), consts.CONST_FLOAT)
+print(type(consts.CONST_LIST), consts.CONST_LIST)
+print(type(consts.CONST_DICT), consts.CONST_DICT)
+print(type(consts.CONST_SET), consts. CONST_SET)
 
 print(dir(consts.WithConsts))
 print(consts.WithConsts)
 print(consts.WithConsts.CONST_TUPLE)
-print(consts.WithConsts.CONST_TUPLE_X)
-print(consts.WithConsts.CONST_TUPLE_X[0])
+print(consts.WithConsts.CONST_LIST)
+print(consts.WithConsts.CONST_DICT)
